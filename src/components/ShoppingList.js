9a90539916a4e4e4a735a3163c9591806dd915f6
Aaron <arnhsmith@gmail.com>
2023-07-05 16:52:50 -0400

    code checked and passed all tests

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  const [selectedCategory, filterCategory] = useState("All");
+
+  const [selectedCategory, filterCategory] = useState("All")
 
   const filteredItems = items.filter((item) => {
     if (selectedCategory === "All") {
@@ -12,16 +13,22 @@ function ShoppingList({ items }) {
     }
   });
 
-  function handleFilterChange(event) {
-    filterCategory(event.target.value);
+  
 
-    console.log(selectedCategory);
+  function handleFilterChange(event) {
+  
+
+    filterCategory(event.target.value);
+    
+    console.log(selectedCategory)
   }
+
+  
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleFilterChange}>
+        <select name="filter" onChange={handleFilterChange} > 
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -30,7 +37,7 @@ function ShoppingList({ items }) {
       </div>
       <ul className="Items">
         {filteredItems.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <Item key={item.id} name={item.name} category={item.category}  />
         ))}
       </ul>
     </div>
